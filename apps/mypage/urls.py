21d1b2fe1e102e7550184767d1ba5a9c84c1fa4f
leesuyong849 <toosee1005@cau.ac.kr>
2025-01-02 17:26:54 +0900

    ADD: mypage new urls

--- apps/mypage/urls.py
+++ apps/mypage/urls.py
@@ -6,7 +6,15 @@ app_name = 'mypage'
 urlpatterns = [
     path('', views.ProfileView.as_view(), name='profile'),
     path('edit/', views.ProfileEditView.as_view(), name='profile_edit'),
-    path('ajax/activities/', views.ActivitiesAjaxView.as_view(), name='ajax_activities'),  # AJAX
+    path('password', views.passeord(), name='password'),                                            #비밀번호 수정
+    path('ajax/scrap', views.scarp(), name='scrap'),                                                #스크랩한 프로필
+    path('ajax/coffeechat/received', views.coffeechat_received(), name='coffeechat_received'),     #커피챗 현환-받은
+    path('ajax/coffeechat/requested', views.coffeechat_requested(), name='coffeechat_requested'),  #커피챗 현황-한
+    path('ajax/coffeechat/in-progress', views.coffeechat_in_progress(), name='coffeechat_in_progress'),     #커피챗 현황-진행중
+    path('ajax/coffeechat/completed', views.coffeechat_completed(), name='coffeechat_completed'),           #커피챗 현황-완료
+    path('memo/<int: pk>', views.memo(), name='coffeechat_memo'),                                           #메모 조회 수정
+
+    path('ajax/activities/', views.ActivitiesAjaxView.as_view(), name='ajax_activities'),
     path('ajax/profile-modal/', views.profile_modal_view, name='profile_modal'),
     path('profile/<int:user_id>/', views.profile_read, name='profile_read'),
     path('bookmark/<int:pk>/', views.coffeechat_bookmark_profile, name='coffeechat_bookmark_profile'),
